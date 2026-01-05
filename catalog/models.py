691f46fa3b68eeb86f6cfe06540e509dcf33ebe1
Inu Oh <ethanol-mace-swan@duck.com>
2026-01-05 00:38:04 +0000

    step 3.1 - added Book model

--- catalog/models.py
+++ catalog/models.py
@@ -26,3 +26,25 @@ class Genre(models.Model):
                 violation_error_message = "Genre already exists (case insenstive match)"
             ),
         ]
+
+
+class Book(models.Model):
+    """Model representing a book (but not a specific copy of a book)."""
+    title = models.Charfield(max_length=200)
+
+    # Author as a string rather than object because it hasn't been declared yet in file.
+    author = models.ForeignKey('Author', on_delete=models.RESTRICT, null=True)
+    summary = models.TextField(max_length=1000, help="Enter a brief description of the book")
+    isbn = models.CharField('ISBN', max_length=13, unique=True,
+        help_text='13 character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+
+    # Genre class has already been defined so we can specify the object.
+    genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
+
+    def __str__(self):
+        """String for representing the book instance."""
+        return self.title
+
+    def get_absolute_url(self):
+        """Returns the URL to access a detail record for a book instance."""
+        return reverse('book-detail', args=[str(self.id)])
