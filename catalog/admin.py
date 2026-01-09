5301b6ab2b9a8013b5f23fd900b570b695b32ad4
Inu Oh <ethanol-mace-swan@duck.com>
2026-01-09 02:03:55 +0000

    ass 4.4 - add inline book list to Author admin view - complete

--- catalog/admin.py
+++ catalog/admin.py
@@ -3,12 +3,24 @@ from django.contrib import admin
 from .models import Author, Book, BookInstance, Genre #, Language
 
 
+class BookInline(admin.StackedInline):
+    """Defines format of inline book insertion (used in AuthorAdmin)"""
+    model = Book
+    extra = 0
+
+
 class AuthorAdmin(admin.ModelAdmin):
-    # Controls display of the admin list view
+    """Administration object for Author models.
+    Defines:
+     - fields to be displayed in list view (list_display)
+     - orders fields in detail view (fields), grouping the date fields horizontally
+     - adds inline addition of books in author view (inlines)
+    """
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
-    # Controls dispaly of the admin detail form. Tuple puts items on one line.
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
     # exclude = ['id'] # will create a form with all fields but id
+
+    inlines = [BookInline]
 
 
 admin.site.register(Author, AuthorAdmin)
@@ -19,12 +31,18 @@ admin.site.register(Author, AuthorAdmin)
 
 
 class BookInstanceInline(admin.TabularInline):
+    """Defines format of inline book instance insertion (used in BookAdmin)"""
     model = BookInstance
     extra = 0
 
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
+    """Administration object for Book models.
+    Defines:
+     - fields to be displayed in list view (list_display)
+     - adds inline addition of book instances in book view (inlines)
+    """
     list_display = ('title', 'author', 'display_genre')
 
     inlines = [BookInstanceInline]
@@ -32,6 +50,12 @@ class BookAdmin(admin.ModelAdmin):
 
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
+    """Administration object for BookInstance models.
+    Defines:
+     - fields to be displayed in list view (list_display)
+     - filters that will be displayed in sidebar (list_filter)
+     - grouping of fields into sections (fieldsets)
+    """
     list_display = ('book', 'status', 'due_back', 'id')
     list_filter = ('status', 'due_back')
 
