72e87eb0cea941b716685b5873b3565419f32177
Inu Oh <ethanol-mace-swan@duck.com>
2026-02-15 18:15:18 +0000

    wiped git commit history and added note

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 from decouple import config
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = config("SECRET_KEY") # referst to secret key in your .env file
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
